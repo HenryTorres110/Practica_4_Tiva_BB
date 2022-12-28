cfd52389dd203e71081a47f288a606c3e2046205
HenryTorres110 <luishenry02@gmail.com>
2022-12-28 13:54:17 -0600

    New

--- lib/PWM.h
+++ lib/PWM.h
@@ -8,7 +8,7 @@
 #ifndef PWM_PWM_H_
 #define PWM_PWM_H_
 
-extern void PWM_CONFIGURATION(int module, int divisor, uint32_t freq, uint32_t f_clk, uint16_t duty_cycle);
+extern void PWM_CONFIGURATION(int module, int divisor, int freq, int f_clk, int duty_cycle);
 
 
 #endif /* PWM_PWM_H_ */
