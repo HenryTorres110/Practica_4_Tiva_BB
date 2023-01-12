93d521217594d8b82025bf6089d65c67672b8107
HenryTorres110 <112783091+HenryTorres110@users.noreply.github.com>
2023-01-11 21:19:51 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -32,24 +32,31 @@ char *inverting_name(char *name, int length){
 int main(void)
 {
     
-    //Configurar_PLL(_50MHZ);  //Configuracion de velocidad de reloj
-    //Configurar_GPIO();
-    //UART_2_CONFIG();
+    Configurar_PLL(_20MHZ);  //Configuracion de velocidad de reloj
+    Configurar_GPIO();
+    UART_2_CONFIG();
     //PWM_CONFIGURATION(0, 0, 10000, 50000000, 50); 
     ADC_CONFIGURATION();
-    SEQ_CONFIGURATION();
-    unsigned int adc_read;   
+    SEQ_CONFIGURATION_0();
+    unsigned int array[10];
     // extern void PWM_CONFIGURATION(int module, int divisor,  int freq, int f_clk, int duty_cycle)
     while (1){
-        ADC0 -> PSSI |= (1 << 3); //| (1 << 0);
-        while ((ADC0 -> RIS & 8) == 0);
-        adc_read = ADC0 -> SSFIFO3;
-        ADC0 -> ISC = 8; // Clearing 0b1000
-        if (adc_read >= 2048){
-            GPIOF -> DATA |= (1 << 1); 
+        ADC1 -> PSSI |= (1 << 0); //| (1 << 0);
+        while ((ADC1 -> RIS & 0x0001) == 0);
+        array[1] = ADC1 -> SSFIFO0;
+        array[2] = ADC1 -> SSFIFO0;
+        array[3] = ADC1 -> SSFIFO0;
+        array[4] = ADC1 -> SSFIFO0;
+        array[5] = ADC1 -> SSFIFO0;
+        ADC1 -> ISC = 0x0001; // Clearing 0b0001
+        if (array[1] < 2040){
+            GPIOF -> DATA |= (1 << 1);
+            GPIOF -> DATA &= ~(1 << 2); 
         }
-        else if (adc_read < 2048){
-            GPIOF -> DATA &= ~(1 << 1); 
+        else if (array[1] > 2040){
+            GPIOF -> DATA &= ~(1 << 1);
+            GPIOF -> DATA |= (1 << 2);  
         }
+
     } 
 }
