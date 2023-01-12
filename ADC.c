9e4258c3c708548c61106054c911fa177aec651a
HenryTorres110 <luishenry02@gmail.com>
2023-01-11 21:15:55 -0600

    H

--- ADC.c
+++ ADC.c
@@ -1,44 +1,53 @@
 #include "lib/include.h"
 
 extern void ADC_CONFIGURATION(void){
-    SYSCTL -> RCGCADC = (1 << 0); // Enable Module 1
-    //SYSCTL -> RCGCADC = (1 << 0); // Enable Module 0
+    SYSCTL -> RCGCADC = (1 << 0); // Enable Module 0
+    SYSCTL -> RCGCADC = (1 << 1); // Enable Module 1
 
-    SYSCTL -> RCGCGPIO = (1 << 4) | (1 << 5); 
-                //       Port E      Port F
-    GPIOE -> DIR &= ~(1 << 1);//(0 << 0) & (0 << 1) & (0 << 2) & (0 << 3) & (0 << 4) & (0 << 5);
-                //      E0       E1        E2           E3          E4       E5 
+    SYSCTL -> RCGCGPIO = (1 << 1) | (1 << 3) |(1 << 4) | (1 << 5); 
+                //        Port B    Port D    Port E      Port F
+    // Configuracion de Pines EN DIRECTION REGISTER DIR para que sean outputs (1) o inputs (0)
+    GPIOE -> DIR &= ~(1 << 2); // AIN1 PE2
+    GPIOE -> DIR &= ~(1 << 5); // AIN8 PE5
+
+    GPIOE -> AFSEL = (1 << 2) | (1 << 5);//0x3F; // E0-E5 AIN0-AIN3, AIN8-AIN9
+    GPIOE -> DEN = ~(1 << 2) | ~(1 << 5); //~0x3F;
+    GPIOE -> AMSEL = (1 << 2) | (1 << 5); //0x3F; 
+
+    GPIOD -> DIR &= ~(1 << 2); // AIN5 PD2
+    GPIOD -> DIR &= ~(1 << 1); // AIN6 PD1
+    GPIOD -> DIR &= ~(1 << 0); // AIN7 PD0
+
+    GPIOD -> AFSEL = (1 << 0) | (1 << 1)| (1 << 2);
+    GPIOD -> DEN = ~(1 << 0) | ~(1 << 1) | ~(1 << 2); 
+    GPIOD -> AMSEL = (1 << 0) | (1 << 1) | (1 << 2);  
+
+    GPIOB -> DIR &= ~(1 << 5); // AIN11 PB5
+    GPIOB -> AFSEL =  (1 << 5);
+    GPIOB -> DEN =  ~(1 << 5); 
+    GPIOB -> AMSEL = (1 << 5); 
     // LED
     GPIOF -> DEN = 0xff; 
     GPIOF -> AFSEL = 0x00; 
     GPIOF -> DIR = 0xff; // Output
     GPIOF -> DATA = (1 << 1); 
-
-    GPIOE -> AFSEL = (1 << 1);//0x3F; // E0-E5 AIN0-AIN3, AIN8-AIN9
-    GPIOE -> DEN = ~(1 << 1); //~0x3F;
-    GPIOE -> AMSEL = (1 << 1); //0x3F; 
 } 
 
-extern void SEQ_CONFIGURATION(void){
-    //ADC0 -> ACTSS &= ~0b1001; // Disable seq 0 and 3
-    ADC0->ACTSS = (0 << 3); //~0x9;
-    // Trigger for sampling
-    //ADC0 -> EMUX = (0xF << 0) | (0xF << 12);
-    ADC0->EMUX = (0xF << 12); //(0xF << 0) | (0xF << 12); // 0xF to sample continuously 
-    
-    ADC0->SSMUX3 = 2; // AN2 CHANNEL
-    //ADC1 -> SSMUX0 = 0x98321;  // AN1-AN3, AN8-AN9 0b10011000001100100001
-
-    ADC0->SSCTL3 = 0x6; //0b0110;
-    ///ADC1 -> SSCTL0 = 0x66666;  // 0b01100110011001100110
-    ADC0->PC = 0x7; // 1 Msps
-
-    ADC0->IM = (1 << 3);  //| (1 << 0);
-
-    // Enable Seq
-    ADC0->ACTSS |= (1 << 3);//0x9;
-    ADC0->ISC = 8; 
+extern void SEQ_CONFIGURATION_0(void){
+    ADC1 -> ACTSS = ~(1 << 0); 
+    ADC1 -> EMUX &= 0x0000F000; 
+    ADC1 -> SSMUX0 = 0xB8765; 
+    ADC1 -> SSCTL0 = 0x66666; 
+    ADC1 -> PC = 0x7; 
+    ADC1 -> IM = (1 << 0); 
+    ADC1 -> ACTSS |= (1 << 0); 
+    ADC1 -> ISC = 0x0001;
+    //ADC0 -> ACTSS = ~(1 << 3); 
+    //ADC0 -> EMUX &= 0x0000F000; 
+    //ADC0 -> SSMUX3 = 1; 
+    //ADC0 -> SSCTL3 = 0x6; 
+    //ADC0 -> PC = 0x7; 
+    //ADC0 -> IM = (1 << 3); 
+    //ADC0 -> ACTSS |= (1 << 3); 
+    //ADC0 -> ISC = 8; 
 }
-
-
-
--- lib/ADC.h
+++ lib/ADC.h
@@ -2,13 +2,13 @@
  * ADC.h
  *
  *  Created on: 04/11/2019
- *      Author: ravenelco
+ *      Author: Henry
  */
 
 #ifndef ADC_ADC_H_
 #define ADC_ADC_H_
 
 extern void ADC_CONFIGURATION(void);
-extern void SEQ_CONFIGURATION(void);
+extern void SEQ_CONFIGURATION_0(void);
 
 #endif /* ADC_ADC_H_ */
